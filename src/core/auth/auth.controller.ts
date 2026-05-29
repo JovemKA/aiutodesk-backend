f5bfc22b60deb17103bf9e865134a0e3288302a3
Andrey Ferreira <andrey.kaiky@outlook.com>
2026-05-28 23:30:57 -0300

    feature: Adicionando reCAPTCHA

--- src/core/auth/auth.controller.ts
+++ src/core/auth/auth.controller.ts
@@ -12,18 +12,24 @@ import { AuthService } from './auth.service';
 import { SignupDto } from './dto/signup.dto';
 import { LoginDto } from './dto/login.dto';
 import { JwtAuthGuard } from './jwt-auth.guard';
+import { RecaptchaGuard } from '@core/recaptcha/recaptcha.guard';
+import { RecaptchaAction } from '@core/recaptcha/recaptcha.decorator';
 
 @Controller('auth')
 export class AuthController {
     constructor(private readonly authService: AuthService) {}
 
     @Post('signup')
+    @UseGuards(RecaptchaGuard)
+    @RecaptchaAction('signup')
     async signup(@Body() dto: SignupDto) {
         return this.authService.signup(dto);
     }
 
     @Post('login')
     @HttpCode(HttpStatus.OK)
+    @UseGuards(RecaptchaGuard)
+    @RecaptchaAction('login')
     async login(@Body() dto: LoginDto) {
         return this.authService.login(dto);
     }
